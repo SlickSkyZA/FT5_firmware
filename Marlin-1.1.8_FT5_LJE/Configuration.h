0f1397150c58bcff8fbdd5626ef855db73c7a807
carefudundae <levi.j.elston@gmail.com>
2018-03-06 20:33:47 -0500

    reduced extruder accel

--- Marlin-1.1.8_FT5_LJE/Configuration.h
+++ Marlin-1.1.8_FT5_LJE/Configuration.h
@@ -559,7 +559,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 200, 10000 }//{ 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 1500, 1500, 200, 2000 }//{ 3000, 3000, 100, 10000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
